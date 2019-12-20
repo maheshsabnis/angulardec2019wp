b2b275121660b561daf385a700cfc315a79e554d
Mahesh Sabnis <sabnis_m@hotmail.com>
2019-12-20 15:20:25 +0530

    Add files via upload

--- src/app/components/categoryComponent/app.category.component.ts
+++ src/app/components/categoryComponent/app.category.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/app.categoey.model';
- 
+import { CommunicationService } from './../../../lib/services/app.coomunication.service'; 
 @Component({
   selector: 'app-category-component',
   templateUrl: './app.category.view.html'
 })
 export class CategoryComponent implements OnInit {
   categories: Array<Category>;
-  constructor() { 
+  // inject the service in comoponent
+
+  constructor(private serv: CommunicationService) { 
     this.categories = new Array<Category>();
   }
 
@@ -15,5 +17,11 @@ export class CategoryComponent implements OnInit {
     this.categories.push(new Category(101, 'Electronics', 100));
     this.categories.push(new Category(102, 'Electrical', 200));
     this.categories.push(new Category(103, 'Food', 300));
+  }
+
+  getSelectedCategory(catName: string): void {
+     // call the method to service and 
+     // pass the catName to it
+     this.serv.onSend(catName);
   }
 }
--- src/app/components/productcomponent/app.product.component.ts
+++ src/app/components/productcomponent/app.product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/app.product.model';
 import { ProductLogic } from '../../models/app.product.logic';
+import { CommunicationService } from '../../../lib/services/app.coomunication.service';
 
 @Component({
   selector: 'app-product-component',
@@ -9,12 +10,36 @@ import { ProductLogic } from '../../models/app.product.logic';
 export class ProductComponent implements OnInit {
   products: Array<Product>;
   private logic: ProductLogic;
-  constructor() { 
+  CatName: string;
+  private filterProducts: Array<Product>;
+  // service injected in the ProductComponent 
+  constructor(private serv: CommunicationService) { 
     this.products = new Array<Product>();
     this.logic = new ProductLogic();
+    this.CatName = "";
+    this.filterProducts = new Array<Product>();
   }
 
   ngOnInit(): void { 
+    // subscribe to the event from the CommunicationService
+    this.serv.notoifyMe.subscribe((data:string) => {
+        this.CatName = data; // receing the data from Event Subscription
+        console.log(`Received data ${this.CatName}`);
+    });
     this.products = this.logic.getData();
   }
+
+  // read-only property that will filter Products data based on CatName
+  // 
+  get FilterProducts(): Array<Product> {
+    this.filterProducts = new Array<Product>();
+    if(this.CatName.length > 0){
+       this.filterProducts = this.products.filter((p,i) => {
+          return p.CatName === this.CatName;
+       });
+    }else {
+      this.filterProducts = this.products;
+    }
+    return this.filterProducts;
+  }  
 }
